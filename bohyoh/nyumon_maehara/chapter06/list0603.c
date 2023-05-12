de16bca727c85c751990b4fbb7bf984b0f9ae19e
kawakubo <tomo.kawakubo@gmail.com>
2023-05-12 17:11:16 +0900

    2023年05月12日

---
+++ bohyoh/nyumon_maehara/chapter06/list0603.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int sqr(int n)
+{
+    return n * n;
+}
+int diff(int a, int b)
+{
+    return a > b ? a - b: b - a;
+}
+int main(void)
+{
+    int x, y;
+    puts("二つの整数を入力せよ。");
+    printf("整数x: "); fscanf(stdin, "%d", &x);
+    printf("整数y: "); fscanf(stdin, "%d", &y);
+    printf("xの2乗とyの2乗の差は%dです。\n", diff(sqr(x), sqr(y)));
+
+    return 0;
+}
