b1534a800933b6685cfbfaf457049cd01a32dd67
kawakubo <tomo.kawakubo@gmail.com>
2023-07-15 19:03:02 +0900

    2023年07月15日授業分

--- bohyoh/nyumon_maehara/chapter09/p0912.c
+++ bohyoh/nyumon_maehara/chapter09/p0912.c
@@ -3,9 +3,36 @@
 
 #define ROW_NUM 100
 #define COL_NUM 128
+
+int str_length(const char s[])
+{
+    int len = 0;
+    while (s[len]) {
+        len++;
+    }
+    return len;
+}
+
+
+void rev_strings(char s[ROW_NUM][COL_NUM])
+{
+    int len;
+    for (int i = 0; i < ROW_NUM; i++) {
+        if (strcmp(s[i], "$$$$$") == 0) {
+            break;
+        }
+        len = str_length(s[i]);
+        for (int j = 0; j < len / 2; j++) {
+            char temp = s[i][j];
+            s[i][j] = s[i][len - 1 - j];
+            s[i][len - 1 - j] = temp;
+        }
+    }
+}
+
 void print_strary(const char s[ROW_NUM][COL_NUM])
 {
-    for (int i = 0; i < ROW_NUM; i++) {
+    for (int i = 0; i < COL_NUM; i++) {
         if (strcmp(s[i], "$$$$$") == 0) {
             break;
         }
@@ -28,6 +55,7 @@ int main(void)
 {
     char str[ROW_NUM][COL_NUM];
     put_strary(str);
+    rev_strings(str);
     puts("-----------------------------");
     print_strary(str);
 
